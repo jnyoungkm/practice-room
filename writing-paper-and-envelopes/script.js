bfb06d3687ee964cb6de174a444957904e362704
jnyoungkm <91418831+jnyoungkm@users.noreply.github.com>
2021-09-26 21:56:34 +0900

    Add files via upload

---
+++ writing-paper-and-envelopes/script.js
@@ -0,0 +1,668 @@
+$('#body').on("click", function(){
+	$(this).css('background-color', 'black')
+})
+
+var randomnum = 0;
+	$("#word1").mouseover(function(){
+		var interval = setInterval(function(){
+
+			randomnum = Math.random() * 0xffffff;
+			randomnum = parseInt(randomnum);
+			randomnum = randomnum.toString(16);
+
+			$("#word1").css("color", "#"+randomnum);
+		}, 50);
+		$("#word1").mouseout(function(){
+			clearInterval(interval);
+		});
+	});
+
+	$("#word2").mouseover(function(){
+		var interval = setInterval(function(){
+
+			randomnum = Math.random() * 0xffffff;
+			randomnum = parseInt(randomnum);
+			randomnum = randomnum.toString(16);
+
+			$("#word2").css("color", "#"+randomnum);
+		}, 50);
+		$("#word2").mouseout(function(){
+			clearInterval(interval);
+		});
+	});
+
+	$("#word3").mouseover(function(){
+		var interval = setInterval(function(){
+
+			randomnum = Math.random() * 0xffffff;
+			randomnum = parseInt(randomnum);
+			randomnum = randomnum.toString(16);
+
+			$("#word3").css("color", "#"+randomnum);
+		}, 50);
+		$("#word3").mouseout(function(){
+			clearInterval(interval);
+		});
+	});
+
+	$("#word4").mouseover(function(){
+		var interval = setInterval(function(){
+
+			randomnum = Math.random() * 0xffffff;
+			randomnum = parseInt(randomnum);
+			randomnum = randomnum.toString(16);
+
+			$("#word4").css("color", "#"+randomnum);
+		}, 50);
+		$("#word4").mouseout(function(){
+			clearInterval(interval);
+		});
+	});
+
+	$("#word5").mouseover(function(){
+		var interval = setInterval(function(){
+
+			randomnum = Math.random() * 0xffffff;
+			randomnum = parseInt(randomnum);
+			randomnum = randomnum.toString(16);
+
+			$("#word5").css("color", "#"+randomnum);
+		}, 50);
+		$("#word5").mouseout(function(){
+			clearInterval(interval);
+		});
+	});
+
+	$("#word6").mouseover(function(){
+		var interval = setInterval(function(){
+
+			randomnum = Math.random() * 0xffffff;
+			randomnum = parseInt(randomnum);
+			randomnum = randomnum.toString(16);
+
+			$("#word6").css("color", "#"+randomnum);
+		}, 50);
+		$("#word6").mouseout(function(){
+			clearInterval(interval);
+		});
+	});
+
+	$("#word7").mouseover(function(){
+		var interval = setInterval(function(){
+
+			randomnum = Math.random() * 0xffffff;
+			randomnum = parseInt(randomnum);
+			randomnum = randomnum.toString(16);
+
+			$("#word7").css("color", "#"+randomnum);
+		}, 50);
+		$("#word7").mouseout(function(){
+			clearInterval(interval);
+		});
+	});
+
+	$("#word8").mouseover(function(){
+		var interval = setInterval(function(){
+
+			randomnum = Math.random() * 0xffffff;
+			randomnum = parseInt(randomnum);
+			randomnum = randomnum.toString(16);
+
+			$("#word8").css("color", "#"+randomnum);
+		}, 50);
+		$("#word8").mouseout(function(){
+			clearInterval(interval);
+		});
+	});
+
+	$("#word9").mouseover(function(){
+		var interval = setInterval(function(){
+
+			randomnum = Math.random() * 0xffffff;
+			randomnum = parseInt(randomnum);
+			randomnum = randomnum.toString(16);
+
+			$("#word9").css("color", "#"+randomnum);
+		}, 50);
+		$("#word9").mouseout(function(){
+			clearInterval(interval);
+		});
+	});
+
+	$("#word10").mouseover(function(){
+		var interval = setInterval(function(){
+
+			randomnum = Math.random() * 0xffffff;
+			randomnum = parseInt(randomnum);
+			randomnum = randomnum.toString(16);
+
+			$("#word10").css("color", "#"+randomnum);
+		}, 50);
+		$("#word10").mouseout(function(){
+			clearInterval(interval);
+		});
+	});
+
+	$("#word11").mouseover(function(){
+		var interval = setInterval(function(){
+
+			randomnum = Math.random() * 0xffffff;
+			randomnum = parseInt(randomnum);
+			randomnum = randomnum.toString(16);
+
+			$("#word11").css("color", "#"+randomnum);
+		}, 50);
+		$("#word11").mouseout(function(){
+			clearInterval(interval);
+		});
+	});
+
+	$("#word12").mouseover(function(){
+		var interval = setInterval(function(){
+
+			randomnum = Math.random() * 0xffffff;
+			randomnum = parseInt(randomnum);
+			randomnum = randomnum.toString(16);
+
+			$("#word12").css("color", "#"+randomnum);
+		}, 50);
+		$("#word12").mouseout(function(){
+			clearInterval(interval);
+		});
+	});
+
+	$("#word13").mouseover(function(){
+		var interval = setInterval(function(){
+
+			randomnum = Math.random() * 0xffffff;
+			randomnum = parseInt(randomnum);
+			randomnum = randomnum.toString(16);
+
+			$("#word13").css("color", "#"+randomnum);
+		}, 50);
+		$("#word13").mouseout(function(){
+			clearInterval(interval);
+		});
+	});
+
+	$("#word14").mouseover(function(){
+		var interval = setInterval(function(){
+
+			randomnum = Math.random() * 0xffffff;
+			randomnum = parseInt(randomnum);
+			randomnum = randomnum.toString(16);
+
+			$("#word14").css("color", "#"+randomnum);
+		}, 50);
+		$("#word14").mouseout(function(){
+			clearInterval(interval);
+		});
+	});
+
+	$("#word15").mouseover(function(){
+		var interval = setInterval(function(){
+
+			randomnum = Math.random() * 0xffffff;
+			randomnum = parseInt(randomnum);
+			randomnum = randomnum.toString(16);
+
+			$("#word15").css("color", "#"+randomnum);
+		}, 150);
+		$("#word15").mouseout(function(){
+			clearInterval(interval);
+		});
+	});
+
+	$("#word16").mouseover(function(){
+		var interval = setInterval(function(){
+
+			randomnum = Math.random() * 0xffffff;
+			randomnum = parseInt(randomnum);
+			randomnum = randomnum.toString(16);
+
+			$("#word16").css("color", "#"+randomnum);
+		}, 50);
+		$("#word16").mouseout(function(){
+			clearInterval(interval);
+		});
+	});
+
+	$("#word17").mouseover(function(){
+		var interval = setInterval(function(){
+
+			randomnum = Math.random() * 0xffffff;
+			randomnum = parseInt(randomnum);
+			randomnum = randomnum.toString(16);
+
+			$("#word17").css("color", "#"+randomnum);
+		}, 50);
+		$("#word17").mouseout(function(){
+			clearInterval(interval);
+		});
+	});
+
+	$("#word18").mouseover(function(){
+		var interval = setInterval(function(){
+
+			randomnum = Math.random() * 0xffffff;
+			randomnum = parseInt(randomnum);
+			randomnum = randomnum.toString(16);
+
+			$("#word18").css("color", "#"+randomnum);
+		}, 50);
+		$("#word18").mouseout(function(){
+			clearInterval(interval);
+		});
+	});
+
+	$("#word19").mouseover(function(){
+		var interval = setInterval(function(){
+
+			randomnum = Math.random() * 0xffffff;
+			randomnum = parseInt(randomnum);
+			randomnum = randomnum.toString(16);
+
+			$("#word19").css("color", "#"+randomnum);
+		}, 50);
+		$("#word19").mouseout(function(){
+			clearInterval(interval);
+		});
+	});
+
+	$("#word20").mouseover(function(){
+		var interval = setInterval(function(){
+
+			randomnum = Math.random() * 0xffffff;
+			randomnum = parseInt(randomnum);
+			randomnum = randomnum.toString(16);
+
+			$("#word20").css("color", "#"+randomnum);
+		}, 50);
+		$("#word20").mouseout(function(){
+			clearInterval(interval);
+		});
+	});
+
+	$("#word21").mouseover(function(){
+		var interval = setInterval(function(){
+
+			randomnum = Math.random() * 0xffffff;
+			randomnum = parseInt(randomnum);
+			randomnum = randomnum.toString(16);
+
+			$("#word21").css("color", "#"+randomnum);
+		}, 50);
+		$("#word21").mouseout(function(){
+			clearInterval(interval);
+		});
+	});
+
+	$("#word22").mouseover(function(){
+		var interval = setInterval(function(){
+
+			randomnum = Math.random() * 0xffffff;
+			randomnum = parseInt(randomnum);
+			randomnum = randomnum.toString(16);
+
+			$("#word22").css("color", "#"+randomnum);
+		}, 50);
+		$("#word22").mouseout(function(){
+			clearInterval(interval);
+		});
+	});
+
+	$("#word23").mouseover(function(){
+		var interval = setInterval(function(){
+
+			randomnum = Math.random() * 0xffffff;
+			randomnum = parseInt(randomnum);
+			randomnum = randomnum.toString(16);
+
+			$("#word23").css("color", "#"+randomnum);
+		}, 50);
+		$("#word23").mouseout(function(){
+			clearInterval(interval);
+		});
+	});
+
+	$("#word24").mouseover(function(){
+		var interval = setInterval(function(){
+
+			randomnum = Math.random() * 0xffffff;
+			randomnum = parseInt(randomnum);
+			randomnum = randomnum.toString(16);
+
+			$("#word24").css("color", "#"+randomnum);
+		}, 50);
+		$("#word24").mouseout(function(){
+			clearInterval(interval);
+		});
+	});
+
+	$("#word25").mouseover(function(){
+		var interval = setInterval(function(){
+
+			randomnum = Math.random() * 0xffffff;
+			randomnum = parseInt(randomnum);
+			randomnum = randomnum.toString(16);
+
+			$("#word25").css("color", "#"+randomnum);
+		}, 50);
+		$("#word25").mouseout(function(){
+			clearInterval(interval);
+		});
+	});
+
+	$("#word26").mouseover(function(){
+		var interval = setInterval(function(){
+
+			randomnum = Math.random() * 0xffffff;
+			randomnum = parseInt(randomnum);
+			randomnum = randomnum.toString(16);
+
+			$("#word26").css("color", "#"+randomnum);
+		}, 50);
+		$("#word26").mouseout(function(){
+			clearInterval(interval);
+		});
+	});
+
+	$("#word27").mouseover(function(){
+		var interval = setInterval(function(){
+
+			randomnum = Math.random() * 0xffffff;
+			randomnum = parseInt(randomnum);
+			randomnum = randomnum.toString(16);
+
+			$("#word27").css("color", "#"+randomnum);
+		}, 50);
+		$("#word27").mouseout(function(){
+			clearInterval(interval);
+		});
+	});
+
+	$("#word28").mouseover(function(){
+		var interval = setInterval(function(){
+
+			randomnum = Math.random() * 0xffffff;
+			randomnum = parseInt(randomnum);
+			randomnum = randomnum.toString(16);
+
+			$("#word28").css("color", "#"+randomnum);
+		}, 50);
+		$("#word28").mouseout(function(){
+			clearInterval(interval);
+		});
+	});
+
+	$("#word29").mouseover(function(){
+		var interval = setInterval(function(){
+
+			randomnum = Math.random() * 0xffffff;
+			randomnum = parseInt(randomnum);
+			randomnum = randomnum.toString(16);
+
+			$("#word29").css("color", "#"+randomnum);
+		}, 50);
+		$("#word29").mouseout(function(){
+			clearInterval(interval);
+		});
+	});
+
+	$("#word30").mouseover(function(){
+		var interval = setInterval(function(){
+
+			randomnum = Math.random() * 0xffffff;
+			randomnum = parseInt(randomnum);
+			randomnum = randomnum.toString(16);
+
+			$("#word30").css("color", "#"+randomnum);
+		}, 50);
+		$("#word30").mouseout(function(){
+			clearInterval(interval);
+		});
+	});
+
+	$("#word31").mouseover(function(){
+		var interval = setInterval(function(){
+
+			randomnum = Math.random() * 0xffffff;
+			randomnum = parseInt(randomnum);
+			randomnum = randomnum.toString(16);
+
+			$("#word31").css("color", "#"+randomnum);
+		}, 50);
+		$("#word31").mouseout(function(){
+			clearInterval(interval);
+		});
+	});
+
+	$("#word32").mouseover(function(){
+		var interval = setInterval(function(){
+
+			randomnum = Math.random() * 0xffffff;
+			randomnum = parseInt(randomnum);
+			randomnum = randomnum.toString(16);
+
+			$("#word32").css("color", "#"+randomnum);
+		}, 50);
+		$("#word32").mouseout(function(){
+			clearInterval(interval);
+		});
+	});
+
+	$("#word33").mouseover(function(){
+		var interval = setInterval(function(){
+
+			randomnum = Math.random() * 0xffffff;
+			randomnum = parseInt(randomnum);
+			randomnum = randomnum.toString(16);
+
+			$("#word33").css("color", "#"+randomnum);
+		}, 50);
+		$("#word33").mouseout(function(){
+			clearInterval(interval);
+		});
+	});
+
+	$("#word34").mouseover(function(){
+		var interval = setInterval(function(){
+
+			randomnum = Math.random() * 0xffffff;
+			randomnum = parseInt(randomnum);
+			randomnum = randomnum.toString(16);
+
+			$("#word34").css("color", "#"+randomnum);
+		}, 50);
+		$("#word34").mouseout(function(){
+			clearInterval(interval);
+		});
+	});
+
+	$("#word35").mouseover(function(){
+		var interval = setInterval(function(){
+
+			randomnum = Math.random() * 0xffffff;
+			randomnum = parseInt(randomnum);
+			randomnum = randomnum.toString(16);
+
+			$("#word35").css("color", "#"+randomnum);
+		}, 50);
+		$("#word35").mouseout(function(){
+			clearInterval(interval);
+		});
+	});
+
+	$("#word36").mouseover(function(){
+		var interval = setInterval(function(){
+
+			randomnum = Math.random() * 0xffffff;
+			randomnum = parseInt(randomnum);
+			randomnum = randomnum.toString(16);
+
+			$("#word36").css("color", "#"+randomnum);
+		}, 50);
+		$("#word36").mouseout(function(){
+			clearInterval(interval);
+		});
+	});
+
+	$("#word37").mouseover(function(){
+		var interval = setInterval(function(){
+
+			randomnum = Math.random() * 0xffffff;
+			randomnum = parseInt(randomnum);
+			randomnum = randomnum.toString(16);
+
+			$("#word37").css("color", "#"+randomnum);
+		}, 50);
+		$("#word37").mouseout(function(){
+			clearInterval(interval);
+		});
+	});
+
+	$("#word38").mouseover(function(){
+		var interval = setInterval(function(){
+
+			randomnum = Math.random() * 0xffffff;
+			randomnum = parseInt(randomnum);
+			randomnum = randomnum.toString(16);
+
+			$("#word38").css("color", "#"+randomnum);
+		}, 50);
+		$("#word38").mouseout(function(){
+			clearInterval(interval);
+		});
+	});
+
+	$("#word39").mouseover(function(){
+		var interval = setInterval(function(){
+
+			randomnum = Math.random() * 0xffffff;
+			randomnum = parseInt(randomnum);
+			randomnum = randomnum.toString(16);
+
+			$("#word39").css("color", "#"+randomnum);
+		}, 50);
+		$("#word39").mouseout(function(){
+			clearInterval(interval);
+		});
+	});
+
+	$("#word40").mouseover(function(){
+		var interval = setInterval(function(){
+
+			randomnum = Math.random() * 0xffffff;
+			randomnum = parseInt(randomnum);
+			randomnum = randomnum.toString(16);
+
+			$("#word40").css("color", "#"+randomnum);
+		}, 50);
+		$("#word40").mouseout(function(){
+			clearInterval(interval);
+		});
+	});
+
+	$("#word41").mouseover(function(){
+		var interval = setInterval(function(){
+
+			randomnum = Math.random() * 0xffffff;
+			randomnum = parseInt(randomnum);
+			randomnum = randomnum.toString(16);
+
+			$("#word41").css("color", "#"+randomnum);
+		}, 50);
+		$("#word41").mouseout(function(){
+			clearInterval(interval);
+		});
+	});
+
+	$("#word42").mouseover(function(){
+		var interval = setInterval(function(){
+
+			randomnum = Math.random() * 0xffffff;
+			randomnum = parseInt(randomnum);
+			randomnum = randomnum.toString(16);
+
+			$("#word42").css("color", "#"+randomnum);
+		}, 50);
+		$("#word42").mouseout(function(){
+			clearInterval(interval);
+		});
+	});
+
+	$("#word43").mouseover(function(){
+		var interval = setInterval(function(){
+
+			randomnum = Math.random() * 0xffffff;
+			randomnum = parseInt(randomnum);
+			randomnum = randomnum.toString(16);
+
+			$("#word43").css("color", "#"+randomnum);
+		}, 50);
+		$("#word43").mouseout(function(){
+			clearInterval(interval);
+		});
+	});
+
+	$("#word44").mouseover(function(){
+		var interval = setInterval(function(){
+
+			randomnum = Math.random() * 0xffffff;
+			randomnum = parseInt(randomnum);
+			randomnum = randomnum.toString(16);
+
+			$("#word44").css("color", "#"+randomnum);
+		}, 50);
+		$("#word44").mouseout(function(){
+			clearInterval(interval);
+		});
+	});
+
+	$("#word45").mouseover(function(){
+		var interval = setInterval(function(){
+
+			randomnum = Math.random() * 0xffffff;
+			randomnum = parseInt(randomnum);
+			randomnum = randomnum.toString(16);
+
+			$("#word45").css("color", "#"+randomnum);
+		}, 50);
+		$("#word45").mouseout(function(){
+			clearInterval(interval);
+		});
+	});
+
+	$("#word46").mouseover(function(){
+		var interval = setInterval(function(){
+
+			randomnum = Math.random() * 0xffffff;
+			randomnum = parseInt(randomnum);
+			randomnum = randomnum.toString(16);
+
+			$("#word46").css("color", "#"+randomnum);
+		}, 50);
+		$("#word46").mouseout(function(){
+			clearInterval(interval);
+		});
+	});
+
+	$("#word47").mouseover(function(){
+		var interval = setInterval(function(){
+
+			randomnum = Math.random() * 0xffffff;
+			randomnum = parseInt(randomnum);
+			randomnum = randomnum.toString(16);
+
+			$("#word47").css("color", "#"+randomnum);
+		}, 50);
+		$("#word47").mouseout(function(){
+			clearInterval(interval);
+		});
+	});
+
+
+
+
+
+
