b6fd1561adcd81b4a8f5de34f93055681bcea95a
jnyoungkm <91418831+jnyoungkm@users.noreply.github.com>
2021-09-26 22:02:20 +0900

    Add files via upload

---
+++ dialogues-with-leuco/이전/script.js
@@ -0,0 +1,177 @@
+$('#title_01').delay(2000).fadeOut(2000);
+
+$('.achilles p:first').delay(4000).fadeIn(2000).click(function(){
+	$('.patroklos p:first').fadeIn(1000).click(function(){
+		$('.achilles p').eq(1).fadeIn(1000).click(function(){
+			$('.patroklos p').eq(1).fadeIn(1000).click(function(){
+				$('.achilles p').eq(2).fadeIn(1000).click(function(){
+					$('.patroklos p').eq(2).fadeIn(1000).click(function(){
+						$('.achilles p').eq(3).fadeIn(1000).click(function(){
+							$('.patroklos p').eq(3).fadeIn(1000).click(function(){
+								$('.achilles p').eq(4).fadeIn(1000).click(function(){
+									$('.patroklos p').eq(4).fadeIn(1000).click(function(){
+										$('.achilles p').eq(5).fadeIn(1000).click(function(){
+											$('.patroklos p').eq(5).fadeIn(1000).click(function(){
+												$('.achilles p').eq(6).fadeIn(1000).click(function(){
+													$('.patroklos p').eq(6).fadeIn(1000).click(function(){
+														$('.achilles p').eq(7).fadeIn(1000).click(function(){
+															$('.patroklos p').eq(7).fadeIn(1000).click(function(){
+																$('.achilles p').eq(8).fadeIn(1000).click(function(){
+																	$('.patroklos p').eq(8).fadeIn(1000).click(function(){
+																		$('.achilles p').eq(9).fadeIn(1000).click(function(){
+																			$('.patroklos p').eq(9).fadeIn(1000).click(function(){
+																				$('.achilles p').eq(10).fadeIn(1000).click(function(){
+																					$('.patroklos p').eq(10).fadeIn(1000).click(function(){
+																						$('.achilles p').eq(11).fadeIn(1000).click(function(){
+																							$('.patroklos p').eq(11).fadeIn(1000).click(function(){
+																								$('.achilles p').eq(12).fadeIn(1000).click(function(){
+																									$('.patroklos p').eq(12).fadeIn(1000).click(function(){
+																										$('.achilles p').eq(13).fadeIn(1000).click(function(){
+																											$('.patroklos p').eq(13).fadeIn(1000).click(function(){
+																												$('.achilles p').eq(14).fadeIn(1000).click(function(){
+																													$('.patroklos p').eq(14).fadeIn(1000).click(function(){
+																														$('.achilles p').eq(15).fadeIn(1000).click(function(){
+																															$('.patroklos p').eq(15).fadeIn(1000).click(function(){
+																																$('.achilles p').eq(16).fadeIn(1000).click(function(){
+																																	$('.patroklos p').eq(16).fadeIn(1000).click(function(){
+																																		$('.achilles p').eq(17).fadeIn(1000).click(function(){
+																																			$('.patroklos p').eq(17).fadeIn(1000).click(function(){
+																																				$('.achilles p').eq(18).fadeIn(1000).click(function(){
+																																					$('.patroklos p').eq(18).fadeIn(1000).click(function(){
+																																						$('.achilles p').eq(19).fadeIn(1000).click(function(){
+																																							$('.patroklos p').eq(19).fadeIn(1000).click(function(){
+																																								$('.achilles p').eq(20).fadeIn(1000).click(function(){
+																																									$('.patroklos p').eq(20).fadeIn(1000).click(function(){
+																																										$('.achilles p').eq(21).fadeIn(1000).click(function(){
+																																											$('.patroklos p').eq(21).fadeIn(1000).click(function(){
+																																												$('.achilles p').eq(22).fadeIn(1000).click(function(){
+																																													$('.patroklos p').eq(22).fadeIn(1000).click(function(){
+																																														$('.achilles p').eq(23).fadeIn(1000).click(function(){
+																																															$('.patroklos p').eq(23).fadeIn(1000).click(function(){
+																																																$('.achilles p').eq(24).fadeIn(1000).click(function(){
+																																																	$('.patroklos p').eq(24).fadeIn(1000).click(function(){
+																																																		$(this).fadeOut(1000);
+																																																	})
+																																																	$(this).fadeOut(1000);
+																																																})
+																																																$(this).fadeOut(1000);
+																																															})
+																																															$(this).fadeOut(1000);
+																																														})
+																																														$(this).fadeOut(1000);
+																																													})
+																																													$(this).fadeOut(1000);
+																																												})
+																																												$(this).fadeOut(1000);
+																																											})
+																																											$(this).fadeOut(1000);
+																																										})
+																																										$(this).fadeOut(1000);
+																																									})
+																																									$(this).fadeOut(1000);
+																																								})
+																																								$(this).fadeOut(1000);
+																																							})
+																																							$(this).fadeOut(1000);
+																																						})
+																																						$(this).fadeOut(1000);
+																																					})
+																																					$(this).fadeOut(1000);
+																																				})
+																																				$(this).fadeOut(1000);
+																																			})
+																																			$(this).fadeOut(1000);
+																																		})
+																																		$(this).fadeOut(1000);
+																																	})
+																																	$(this).fadeOut(1000);
+																																})
+																																$(this).fadeOut(1000);
+																															})
+																															$(this).fadeOut(1000);
+																														})
+																														$(this).fadeOut(1000);
+																													})
+																													$(this).fadeOut(1000);
+																												})
+																												$(this).fadeOut(1000);
+																											})
+																											$(this).fadeOut(1000);
+																										})
+																										$(this).fadeOut(1000);
+																									})
+																									$(this).fadeOut(1000);
+																								})
+																								$(this).fadeOut(1000);
+																							})
+																							$(this).fadeOut(1000);
+																						})
+																						$(this).fadeOut(1000);
+																					})
+																					$(this).fadeOut(1000);
+																				})
+																				$(this).fadeOut(1000);
+																			})
+																			$(this).fadeOut(1000);
+																		})
+																		$(this).fadeOut(1000);
+																	})
+																	$(this).fadeOut(1000);
+																})
+																$(this).fadeOut(1000);
+															})
+															$(this).fadeOut(1000);
+														})
+														$(this).fadeOut(1000);
+													});
+													$(this).fadeOut(1000);
+												});
+												$(this).fadeOut(1000);
+											});
+											$(this).fadeOut(1000);
+										});
+										$(this).fadeOut(1000);
+									});
+									$(this).fadeOut(1000);
+								});
+								$(this).fadeOut(1000);
+							});
+							$(this).fadeOut(1000);
+						});
+						$(this).fadeOut(1000);
+					});
+					$(this).fadeOut(1000);
+				});
+				$(this).fadeOut(1000);
+			});
+			$(this).fadeOut(1000);
+		});
+		$(this).fadeOut(1000);
+	});
+	$(this).fadeOut(1000);
+});
+
+
+$('#first').mouseover(function(){
+	$('#first-text').show();
+})
+
+$('#first').mouseleave(function(){
+	$('#first-text').hide();
+})
+
+$('#second').mouseover(function(){
+	$('#second-text').show();
+})
+
+$('#second').mouseleave(function(){
+	$('#second-text').hide();
+})
+
+$('#third').mouseover(function(){
+	$('#third-text').show();
+})
+
+$('#third').mouseleave(function(){
+	$('#third-text').hide();
+})
