89a091750cf1c042c227cbbb23abc52c4aa2baaf
jnyoungkm <91418831+jnyoungkm@users.noreply.github.com>
2021-09-26 22:01:23 +0900

    Add files via upload

---
+++ color-name-list/script.js
@@ -0,0 +1,567 @@
+$('#AliceBlue').on("click", function(){
+	$('body').css('background-color', 'AliceBlue');
+})
+
+$('#AntiqueWhite').on("click", function(){
+	$('body').css('background-color', 'AntiqueWhite');
+})
+
+$('#Aqua').on("click", function(){
+	$('body').css('background-color', 'Aqua');
+})
+
+$('#Aquamarine').on("click", function(){
+	$('body').css('background-color', 'Aquamarine');
+})
+
+$('#Azure').on("click", function(){
+	$('body').css('background-color', 'Azure');
+})
+
+$('#Beige').on("click", function(){
+	$('body').css('background-color', 'Beige');
+})
+
+$('#Bisque').on("click", function(){
+	$('body').css('background-color', 'Bisque');
+})
+
+$('#Black').on("click", function(){
+	$('body').css('background-color', 'Black');
+})
+
+$('#Blanched-Almond').on("click", function(){
+	$('body').css('background-color', 'BlanchedAlmond');
+})
+
+$('#Blue').on("click", function(){
+	$('body').css('background-color', 'Blue');
+})
+
+$('#Blue-Violet').on("click", function(){
+	$('body').css('background-color', 'BlueViolet');
+})
+
+$('#Brown').on("click", function(){
+	$('body').css('background-color', 'Brown');
+})
+
+$('#Burlywood').on("click", function(){
+	$('body').css('background-color', 'Burlywood');
+})
+
+$('#Cadet-Blue').on("click", function(){
+	$('body').css('background-color', 'CadetBlue');
+})
+
+$('#Chartreuse').on("click", function(){
+	$('body').css('background-color', 'Chartreuse');
+})
+
+$('#Chocolate').on("click", function(){
+	$('body').css('background-color', 'Chocolate');
+})
+
+$('#Coral').on("click", function(){
+	$('body').css('background-color', 'Coral');
+})
+
+$('#CornflowerBlue').on("click", function(){
+	$('body').css('background-color', 'CornflowerBlue');
+})
+
+$('#Cornsilk').on("click", function(){
+	$('body').css('background-color', 'Cornsilk');
+})
+
+$('#Crimson').on("click", function(){
+	$('body').css('background-color', 'Crimson');
+})
+
+$('#Cyan').on("click", function(){
+	$('body').css('background-color', 'Cyan');
+})
+
+$('#DarkBlue').on("click", function(){
+	$('body').css('background-color', 'DarkBlue');
+})
+
+$('#DarkCyan').on("click", function(){
+	$('body').css('background-color', 'DarkCyan');
+})
+
+$('#DarkGoldenRod').on("click", function(){
+	$('body').css('background-color', 'DarkGoldenRod');
+})
+
+$('#DarkGray').on("click", function(){
+	$('body').css('background-color', 'DarkGray');
+})
+
+$('#DarkGreen').on("click", function(){
+	$('body').css('background-color', 'DarkGreen');
+})
+
+$('#DarkKhaki').on("click", function(){
+	$('body').css('background-color', 'DarkKhaki');
+})
+
+$('#DarkMagenta').on("click", function(){
+	$('body').css('background-color', 'DarkMagenta');
+})
+
+$('#DarkOliveGreen').on("click", function(){
+	$('body').css('background-color', 'DarkOliveGreen');
+})
+
+$('#DarkOrange').on("click", function(){
+	$('body').css('background-color', 'DarkOrange');
+})
+
+$('#DarkOrchid').on("click", function(){
+	$('body').css('background-color', 'DarkOrchid');
+})
+
+$('#DarkRed').on("click", function(){
+	$('body').css('background-color', 'DarkRed');
+})
+
+$('#DarkSalmon').on("click", function(){
+	$('body').css('background-color', 'DarkSalmon');
+})
+
+$('#DarkSeaGreen').on("click", function(){
+	$('body').css('background-color', 'DarkSeaGreen');
+})
+
+$('#DarkSlateBlue').on("click", function(){
+	$('body').css('background-color', 'DarkSlateBlue');
+})
+
+$('#DarkSlateGray').on("click", function(){
+	$('body').css('background-color', 'DarkSlateGray');
+})
+
+$('#DarkTurquoise').on("click", function(){
+	$('body').css('background-color', 'DarkTurquoise');
+})
+
+$('#DarkViolet').on("click", function(){
+	$('body').css('background-color', 'DarkViolet');
+})
+
+$('#DeepPink').on("click", function(){
+	$('body').css('background-color', 'DeepPink');
+})
+
+$('#DeepSkyBlue').on("click", function(){
+	$('body').css('background-color', 'DeepSkyBlue');
+})
+
+$('#DimGray').on("click", function(){
+	$('body').css('background-color', 'DimGray');
+})
+
+$('#DodgerBlue').on("click", function(){
+	$('body').css('background-color', 'DodgerBlue');
+})
+
+$('#FireBrick').on("click", function(){
+	$('body').css('background-color', 'FireBrick');
+})
+
+$('#FloralWhite').on("click", function(){
+	$('body').css('background-color', 'FloralWhite');
+})
+
+$('#ForestGreen').on("click", function(){
+	$('body').css('background-color', 'ForestGreen');
+})
+
+$('#Fuchsia').on("click", function(){
+	$('body').css('background-color', 'Fuchsia');
+})
+
+$('#Gainsboro').on("click", function(){
+	$('body').css('background-color', 'Gainsboro');
+})
+
+$('#GhostWhite').on("click", function(){
+	$('body').css('background-color', 'GhostWhite');
+})
+
+$('#Gold').on("click", function(){
+	$('body').css('background-color', 'Gold');
+})
+
+$('#GoldenRod').on("click", function(){
+	$('body').css('background-color', 'GoldenRod');
+})
+
+$('#Gray').on("click", function(){
+	$('body').css('background-color', 'Gray');
+})
+
+$('#Green').on("click", function(){
+	$('body').css('background-color', 'Green');
+})
+
+$('#GreenYellow').on("click", function(){
+	$('body').css('background-color', 'GreenYellow');
+})
+
+$('#HoneyDew').on("click", function(){
+	$('body').css('background-color', 'HoneyDew');
+})
+
+$('#HotPink').on("click", function(){
+	$('body').css('background-color', 'HotPink');
+})
+
+$('#IndianRed').on("click", function(){
+	$('body').css('background-color', 'IndianRed');
+})
+
+$('#Indigo').on("click", function(){
+	$('body').css('background-color', 'Indigo');
+})
+
+$('#Ivory').on("click", function(){
+	$('body').css('background-color', 'Ivory');
+})
+
+$('#Khaki').on("click", function(){
+	$('body').css('background-color', 'Khaki');
+})
+
+$('#Lavender').on("click", function(){
+	$('body').css('background-color', 'Lavender');
+})
+
+$('#LavenderBlush').on("click", function(){
+	$('body').css('background-color', 'LavenderBlush');
+})
+
+$('#LawnGreen').on("click", function(){
+	$('body').css('background-color', 'LawnGreen');
+})
+
+$('#LemonChiffon').on("click", function(){
+	$('body').css('background-color', 'LemonChiffon');
+})
+
+$('#LightBlue').on("click", function(){
+	$('body').css('background-color', 'LightBlue');
+})
+
+$('#LightCoral').on("click", function(){
+	$('body').css('background-color', 'LightCoral');
+})
+
+$('#LightCyan').on("click", function(){
+	$('body').css('background-color', 'LightCyan');
+})
+
+$('#LightGoldenRodYellow').on("click", function(){
+	$('body').css('background-color', 'LightGoldenRodYellow');
+})
+
+$('#LightGray').on("click", function(){
+	$('body').css('background-color', 'LightGray');
+})
+
+$('#LightGreen').on("click", function(){
+	$('body').css('background-color', 'LightGreen');
+})
+
+$('#LightPink').on("click", function(){
+	$('body').css('background-color', 'LightPink');
+})
+
+$('#LightSalmon').on("click", function(){
+	$('body').css('background-color', 'LightSalmon');
+})
+
+$('#LightSeaGreen').on("click", function(){
+	$('body').css('background-color', 'LightSeaGreen');
+})
+
+$('#LightSkyBlue').on("click", function(){
+	$('body').css('background-color', 'LightSkyBlue');
+})
+
+$('#LightSlateGray').on("click", function(){
+	$('body').css('background-color', 'LightSlateGray');
+})
+
+$('#LightSteelBlue').on("click", function(){
+	$('body').css('background-color', 'LightSteelBlue');
+})
+
+$('#LightYellow').on("click", function(){
+	$('body').css('background-color', 'LightYellow');
+})
+
+$('#Lime').on("click", function(){
+	$('body').css('background-color', 'Lime');
+})
+
+$('#LimeGreen').on("click", function(){
+	$('body').css('background-color', 'LimeGreen');
+})
+
+$('#Linen').on("click", function(){
+	$('body').css('background-color', 'Linen');
+})
+
+
+
+
+
+
+
+
+$('#Magenta').on("click", function(){
+	$('body').css('background-color', 'Magenta');
+})
+
+$('#Maroon').on("click", function(){
+	$('body').css('background-color', 'Maroon');
+})
+
+$('#MediumAquaMarine').on("click", function(){
+	$('body').css('background-color', 'MediumAquaMarine');
+})
+
+$('#MediumBlue').on("click", function(){
+	$('body').css('background-color', 'MediumBlue');
+})
+
+$('#MediumOrchid').on("click", function(){
+	$('body').css('background-color', 'MediumOrchid');
+})
+
+$('#MediumPurple').on("click", function(){
+	$('body').css('background-color', 'MediumPurple');
+})
+
+$('#MediumSeaGreen').on("click", function(){
+	$('body').css('background-color', 'MediumSeaGreen');
+})
+
+$('#MediumSlateBlue').on("click", function(){
+	$('body').css('background-color', 'MediumSlateBlue');
+})
+
+$('#MediumSpringGreen').on("click", function(){
+	$('body').css('background-color', 'MediumSpringGreen');
+})
+
+$('#MediumTurquoise').on("click", function(){
+	$('body').css('background-color', 'MediumTurquoise');
+})
+
+$('#MediumVioletRed').on("click", function(){
+	$('body').css('background-color', 'MediumVioletRed');
+})
+
+$('#MidnightBlue').on("click", function(){
+	$('body').css('background-color', 'MidnightBlue');
+})
+
+$('#MintCream').on("click", function(){
+	$('body').css('background-color', 'MintCream');
+})
+
+$('#MistyRose').on("click", function(){
+	$('body').css('background-color', 'MistyRose');
+})
+
+$('#Moccasin').on("click", function(){
+	$('body').css('background-color', 'Moccasin');
+})
+
+$('#NavajoWhite').on("click", function(){
+	$('body').css('background-color', 'NavajoWhite');
+})
+
+$('#NavyBlue').on("click", function(){
+	$('body').css('background-color', 'NavyBlue');
+})
+
+$('#OldLace').on("click", function(){
+	$('body').css('background-color', 'OldLace');
+})
+
+$('#Olive').on("click", function(){
+	$('body').css('background-color', 'Olive');
+})
+
+$('#OliveDrab').on("click", function(){
+	$('body').css('background-color', 'OliveDrab');
+})
+
+$('#Orange').on("click", function(){
+	$('body').css('background-color', 'Orange');
+})
+
+$('#OrangeRed').on("click", function(){
+	$('body').css('background-color', 'OrangeRed');
+})
+
+$('#Orchid').on("click", function(){
+	$('body').css('background-color', 'Orchid');
+})
+
+$('#PaleGoldenRod').on("click", function(){
+	$('body').css('background-color', 'PaleGoldenRod');
+})
+
+$('#PaleGreen').on("click", function(){
+	$('body').css('background-color', 'PaleGreen');
+})
+
+$('#PaleTurquoise').on("click", function(){
+	$('body').css('background-color', 'PaleTurquoise');
+})
+
+$('#PaleVioletRed').on("click", function(){
+	$('body').css('background-color', 'PaleVioletRed');
+})
+
+$('#PapayaWhip').on("click", function(){
+	$('body').css('background-color', 'PapayaWhip');
+})
+
+$('#PeachPuff').on("click", function(){
+	$('body').css('background-color', 'PeachPuff');
+})
+
+$('#Peru').on("click", function(){
+	$('body').css('background-color', 'Peru');
+})
+
+$('#Pink').on("click", function(){
+	$('body').css('background-color', 'Pink');
+})
+
+$('#Plum').on("click", function(){
+	$('body').css('background-color', 'Plum');
+})
+
+$('#PowderBlue').on("click", function(){
+	$('body').css('background-color', 'PowderBlue');
+})
+
+$('#Purple').on("click", function(){
+	$('body').css('background-color', 'Purple');
+})
+
+$('#Red').on("click", function(){
+	$('body').css('background-color', 'Red');
+})
+
+$('#RosyBrown').on("click", function(){
+	$('body').css('background-color', 'RosyBrown');
+})
+
+$('#RoyalBlue').on("click", function(){
+	$('body').css('background-color', 'RoyalBlue');
+})
+
+$('#SaddleBrown').on("click", function(){
+	$('body').css('background-color', 'SaddleBrown');
+})
+
+$('#Salmon').on("click", function(){
+	$('body').css('background-color', 'Salmon');
+})
+
+$('#SandyBrown').on("click", function(){
+	$('body').css('background-color', 'SandyBrown');
+})
+
+$('#SeaGreen').on("click", function(){
+	$('body').css('background-color', 'SeaGreen');
+})
+
+$('#SeaShell').on("click", function(){
+	$('body').css('background-color', 'SeaShell');
+})
+
+$('#Sienna').on("click", function(){
+	$('body').css('background-color', 'Sienna');
+})
+
+$('#Silver').on("click", function(){
+	$('body').css('background-color', 'Silver');
+})
+
+$('#SkyBlue').on("click", function(){
+	$('body').css('background-color', 'SkyBlue');
+})
+
+$('#SlateBlue').on("click", function(){
+	$('body').css('background-color', 'SlateBlue');
+})
+
+$('#SlateGray').on("click", function(){
+	$('body').css('background-color', 'SlateGray');
+})
+
+$('#Snow').on("click", function(){
+	$('body').css('background-color', 'Snow');
+})
+
+$('#SpringGreen').on("click", function(){
+	$('body').css('background-color', 'SpringGreen');
+})
+
+$('#SteelBlue').on("click", function(){
+	$('body').css('background-color', 'SteelBlue');
+})
+
+$('#Tan').on("click", function(){
+	$('body').css('background-color', 'Tan');
+})
+
+$('#Teal').on("click", function(){
+	$('body').css('background-color', 'Teal');
+})
+
+$('#Thistle').on("click", function(){
+	$('body').css('background-color', 'Thistle');
+})
+
+$('#Tomato').on("click", function(){
+	$('body').css('background-color', 'Tomato');
+})
+
+$('#Turquoise').on("click", function(){
+	$('body').css('background-color', 'Turquoise');
+})
+
+$('#Violet').on("click", function(){
+	$('body').css('background-color', 'Violet');
+})
+
+$('#Wheat').on("click", function(){
+	$('body').css('background-color', 'Wheat');
+})
+
+$('#White').on("click", function(){
+	$('body').css('background-color', 'White');
+})
+
+$('#WhiteSmoke').on("click", function(){
+	$('body').css('background-color', 'WhiteSmoke');
+})
+
+$('#Yellow').on("click", function(){
+	$('body').css('background-color', 'Yellow');
+})
+
+$('#YellowGreen').on("click", function(){
+	$('body').css('background-color', 'YellowGreen');
+})
+
